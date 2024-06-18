57d06c9eab8294c6cb4eb5b0357b64e3252a06d8
Quide1 <ezequielr.dev@gmail.com>
2024-06-18 18:12:58 -0300

    Add icons

--- src/components/AlarmPreferences.tsx
+++ src/components/AlarmPreferences.tsx
@@ -7,20 +7,23 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import { SoundRoutes } from "@/constants/soundRoutes";
+import SpeakerIcon from "@/icons/SpeakerIcon";
+
 function AlarmPreferences() {
     return (
         <div className="flex flex-row  items-center justify-between gap-2 ">
             <Label>Elegir sonido de alarma</Label>
-            <Select onValueChange={() => { console.log('asdsads') }}>
+            <Select onValueChange={() => { console.log('asdsads') }} defaultValue="">
                 <SelectTrigger className="w-[180px]">
                     <SelectValue placeholder="Alarma" />
                 </SelectTrigger>
                 <SelectContent >
-                    {SoundRoutes.map((value,index) => {
+                    {SoundRoutes.map((value, index) => {
                         return <SelectItem key={index} value={value.path}>{value.label}</SelectItem>;
                     })}
                 </SelectContent>
             </Select>
+            <SpeakerIcon className="w-8 rounded-sm p-1 h-auto border-1 bg-slate-300 border-red-600"></SpeakerIcon>
         </div>
     );
 }
--- src/components/FooterTimerCard.tsx
+++ src/components/FooterTimerCard.tsx
@@ -1,15 +1,17 @@
 import { CardFooter } from "@/components/ui/card";
 import { Button } from "./ui/button";
 import { useTimerFunctionalities } from "@/hooks/useTimerFunctionalities";
-
+import PlayIcon from "@/icons/PlayIcon";
+import PauseIcon from "@/icons/PauseIcon";
 function FooterTimerCard() {
   const { startTimer, timerStatus, pauseTimer } = useTimerFunctionalities();
   return (
     <CardFooter>
       {timerStatus.isRunning ? (
-        <Button onClick={pauseTimer}>Pausar</Button>
+        <Button onClick={pauseTimer}>
+         Pausar <PauseIcon className={'ml-2'}/></Button>
       ) : (
-        <Button onClick={startTimer}>Comenzar</Button>
+        <Button onClick={startTimer}>Comenzar  <PlayIcon className="ml-2"/></Button>
       )}
     </CardFooter>
   );
