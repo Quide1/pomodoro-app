6938243a0ed6c34f28e745201c19ad784d3792a8
Quide1 <ezequielr.dev@gmail.com>
2024-06-18 22:44:09 -0300

    Add new methods for tasks

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,13 +1,46 @@
-import { Task } from "@/types/tasks";
+import { Task, TaskStatus } from "@/types/tasks";
 import { create } from "zustand";
 
 interface AppState {
-    Tasks:Task[],
-    CreateTask: ()=>Promise<void>
-    ToggleProgress:()=>Promise<void>,
-    EliminateTasks:()=>Promise<void>
-
+    tasks: Task[];
+    createTask: (newTask: Task) => Promise<void>;
+    toggleProgress: (index: number) => Promise<void>;
+    eliminateTasksDone: () => Promise<void>;
+    eliminateOneTask: (indexToEliminate: number) => Promise<void>;
 }
-export const useTaskStore = create<AppState>((set,get)=>({
 
-}))
+const initialTasks: Task[] = [
+    { description: "Inicia un pomodoro!", status: TaskStatus.ToDo },
+];
+
+export const useTaskStore = create<AppState>((set, get) => ({
+    tasks: initialTasks,
+    createTask: async (newTask) => {
+        const { tasks } = get();
+        const newTasks = [...tasks, newTask];
+        set({ tasks: newTasks });
+    },
+    toggleProgress: async (index) => {
+        const { tasks } = get();
+        const newTasks = tasks.map((task, i) => {
+            if (i === index) {
+                return {
+                    ...task,
+                    status: task.status === TaskStatus.ToDo ? TaskStatus.Done : TaskStatus.ToDo,
+                };
+            }
+            return task;
+        });
+        set({ tasks: newTasks });
+    },
+    eliminateTasksDone: async () => {
+        const { tasks } = get();
+        const newTasks = tasks.filter((task) => task.status !== TaskStatus.Done);
+        set({ tasks: newTasks });
+    },
+    eliminateOneTask: async (indexToEliminate) => {
+        const { tasks } = get();
+        const newTasks = tasks.filter((_task, index) => index !== indexToEliminate);
+        set({ tasks: newTasks });
+    }
+}));
