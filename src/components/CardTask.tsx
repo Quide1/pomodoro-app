910e2c12846cb65875742d3090453d60ce6e7e0d
Quide1 <ezequielr.dev@gmail.com>
2024-06-19 23:46:22 -0300

    change styles

--- src/components/CardTask.tsx
+++ src/components/CardTask.tsx
@@ -3,6 +3,8 @@ import { TaskStatus, type Task } from "@/types/tasks";
 import CircleIcon from "@/icons/CircleIcon";
 import CheckedIcon from "@/icons/CheckedIcon";
 import { useTaskFunctionalities } from "@/hooks/useTaskFunctionalities";
+import ThrashIcon from "@/icons/ThrashIcon";
+import PencilIcon from "@/icons/PencilIcon";
 
 type TaskProps = Task & {
     index: number
@@ -16,20 +18,19 @@ function CardTask({ description, status, index }: TaskProps) {
         toggleProgress(index)
     }
     return (
-        <Card className="w-full max-w-2xl border-2 border-black">
-            <CardContent className={`flex items-center justify-start p-4 ${status === TaskStatus.Done ? 'bg-slate-800' : 'bg-slate-600'}`}>
+        <Card className={`w-full max-w-2xl border-2 border-black ${status === TaskStatus.Done ? 'bg-slate-800' : 'bg-slate-600'}`}>
+            <CardContent className={`flex items-center justify-start p-4 `}>
                 <div onClick={toggleProgressHandler} className="flex items-center gap-3">
                     {
                         status === TaskStatus.ToDo ? <CircleIcon /> : <CheckedIcon />
                     }
                 </div>
-                <CardDescription className={`text-white ml-4 ${status === TaskStatus.Done ? 'line-through' : ''}`}>
+                <CardDescription className={`text-white m-4 text-ellipsis ${status === TaskStatus.Done ? 'line-through' : ''}`}>
                     {description}
                 </CardDescription>
-                <div  className="flex items-center gap-3">
-                    {
-                        status === TaskStatus.ToDo ? <CircleIcon /> : <CheckedIcon />
-                    }
+                <div className="flex flex-col gap-4">
+                    <ThrashIcon className="w-5 h-5 cursor-pointer" />
+                    <PencilIcon className="w-5 h-5 cursor-pointer" />
                 </div>
             </CardContent>
         </Card>
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -5,18 +5,20 @@ import CardTAsk from "./CardTask";
 import ThrashIcon from "@/icons/ThrashIcon";
 
 function Tasks() {
-    const { tasks } = useTaskFunctionalities()
+    const { tasks,eliminateTasksDone } = useTaskFunctionalities()
+    const onClickDeleteTaskDone = () =>{
+        eliminateTasksDone()
+    }
     return (
         <Card className=" w-full max-w-[500px] flex flex-col items-center mt-2">
             <CardHeader className="flex flex-row items-center justify-center">
-                <Button>
+                <Button onClick={onClickDeleteTaskDone}>
                     <ThrashIcon className="mr-2 h-4 w-4" /> Eliminar hechas
                 </Button>
             </CardHeader>
             <CardContent>
                 {
                     tasks.map((task, index) => {
-                        console.log(task)
                         return <CardTAsk key={index} description={task.description} status={task.status} index={index} ></CardTAsk>
                     })
                 }
