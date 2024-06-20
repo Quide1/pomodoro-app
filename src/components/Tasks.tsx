21a3f5c30eebe1196fd5446012d2ea9cf5f265aa
Quide1 <ezequielr.dev@gmail.com>
2024-06-19 22:30:14 -0300

    Add index to props

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -9,17 +9,15 @@ function Tasks() {
     return (
         <Card className=" w-full max-w-[500px] flex flex-col items-center mt-2">
             <CardHeader className="flex flex-row items-center justify-center">
-                <Button variant="outline" size="icon">
-                    Eliminar hechas
-                    <ThrashIcon />
-
+                <Button>
+                    <ThrashIcon className="mr-2 h-4 w-4" /> Eliminar hechas
                 </Button>
             </CardHeader>
             <CardContent>
                 {
                     tasks.map((task, index) => {
                         console.log(task)
-                        return <CardTAsk key={index} description={task.description} status={task.status} ></CardTAsk>
+                        return <CardTAsk key={index} description={task.description} status={task.status} index={index} ></CardTAsk>
                     })
                 }
             </CardContent>
