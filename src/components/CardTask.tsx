70ad96ab19c08a59cfd21105090961e156d77d4f
Quide1 <ezequielr.dev@gmail.com>
2024-06-19 22:30:03 -0300

    Add toggle status a card

--- src/components/CardTask.tsx
+++ src/components/CardTask.tsx
@@ -1,20 +1,39 @@
-import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { type Task } from "@/types/tasks";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import { TaskStatus, type Task } from "@/types/tasks";
+import CircleIcon from "@/icons/CircleIcon";
+import CheckedIcon from "@/icons/CheckedIcon";
+import { useTaskFunctionalities } from "@/hooks/useTaskFunctionalities";
 
-function CardTAsk({ description, status }: Task) {
-    return (
-        <Card className=" w-full mt-2 bg-gray-600">
-            <CardHeader className="flex flex-row items-center justify-center">
-            </CardHeader>
-            <CardContent>
-                <p>{description}</p>
-            </CardContent>
-            <CardFooter>
-
-            </CardFooter>
-        </Card>
-    )
+type TaskProps = Task & {
+    index: number
 }
 
-export default CardTAsk
+
+
+function CardTask({ description, status, index }: TaskProps) {
+    const { toggleProgress } = useTaskFunctionalities()
+    const toggleProgressHandler = () => {
+        toggleProgress(index)
+    }
+    return (
+        <Card className="w-full max-w-2xl border-2 border-black">
+            <CardContent className={`flex items-center justify-start p-4 ${status === TaskStatus.Done ? 'bg-slate-800' : 'bg-slate-600'}`}>
+                <div onClick={toggleProgressHandler} className="flex items-center gap-3">
+                    {
+                        status === TaskStatus.ToDo ? <CircleIcon /> : <CheckedIcon />
+                    }
+                </div>
+                <CardDescription className={`text-white ml-4 ${status === TaskStatus.Done ? 'line-through' : ''}`}>
+                    {description}
+                </CardDescription>
+                <div  className="flex items-center gap-3">
+                    {
+                        status === TaskStatus.ToDo ? <CircleIcon /> : <CheckedIcon />
+                    }
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default CardTask;
