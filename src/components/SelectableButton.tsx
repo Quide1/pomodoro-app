8cf62783c5f4f304e5e70a75c8d6d36515421484
Quide1 <ezequielr.dev@gmail.com>
2024-06-22 06:58:22 -0300

    Add new background

--- src/components/SelectableButton.tsx
+++ src/components/SelectableButton.tsx
@@ -5,13 +5,13 @@ type SelectableButtonProps = {
   label: string;
   isSelected: boolean;
   onClick: () => void;
-  bgColor:string;
+  bgColor: string;
 };
 
-const SelectableButton: React.FC<SelectableButtonProps> = ({ label, isSelected, onClick,bgColor }) => {
+const SelectableButton: React.FC<SelectableButtonProps> = ({ label, isSelected, onClick, bgColor }) => {
   return (
     <button
-      className={`px-1 py-1 border text-clip ${isSelected ? 'bg-blue-500 text-white' : 'bg-white text-black'} ${bgColor}`}
+      className={`p-1 border text-clip ${isSelected ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black'} rounded-sm ${bgColor}`}
       onClick={onClick}
     >
       {label}
