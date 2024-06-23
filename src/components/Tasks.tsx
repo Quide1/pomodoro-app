f41383eabac563361a76f67447ba83333d92f5c3
Quide1 <ezequielr.dev@gmail.com>
2024-06-22 23:52:23 -0300

    Add scroll area

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,8 +1,9 @@
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { useTaskFunctionalities } from "@/hooks/useTaskFunctionalities";
 import CardTAsk from "./CardTask";
 import DeleteTaskDone from "./DeleteTaskDone";
 import CreateNewTask from "./CreateNewTask";
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 function Tasks() {
     const { tasks } = useTaskFunctionalities()
@@ -11,19 +12,20 @@ function Tasks() {
         <Card className=" w-full max-w-[500px] flex flex-col items-center mt-6 bg-gray-400 border-white bg-opacity-20">
             <div className="flex flex-row flex-wrap gap-4 w-full items-center justify-center p-2 m-2">
                 <DeleteTaskDone />
-                <CreateNewTask/>
+                <CreateNewTask />
             </div>
-            <CardContent className="flex flex-col gap-4 w-full items-center">
-                {
-                    tasks.length> 0 ? 
-                    tasks.map((task, index) => {
-                        return <CardTAsk key={index} description={task.description} status={task.status} index={index} ></CardTAsk>
-                    })  : <p className="text-gray-600">Sin tareas pendientes</p>
-                }
-            </CardContent>
-            <CardFooter>
+            <ScrollArea className=" h-[500px]">
+                <CardContent className="flex flex-col gap-4 w-full items-center ">
+                    {
+                        tasks.length > 0 ?
+                            tasks.map((task, index) => {
+                                return <CardTAsk key={index} description={task.description} status={task.status} index={index} ></CardTAsk>
+                            }) : <p className="text-gray-600">Sin tareas pendientes</p>
+                    }
+                </CardContent>
+            </ScrollArea>
 
-            </CardFooter>
+
         </Card>
     )
 }
