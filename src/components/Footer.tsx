5c5d67cf0e92fa61fb159f1553a3a4731a72ce75
Quide1 <ezequielr.dev@gmail.com>
2024-06-23 05:36:20 -0300

    Create a footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import GitHubIcon from '@/icons/GitHubIcon'
+import LinkedInIcon from '@/icons/LinkedinIcon'
+
+function Footer() {
+    return (
+        <footer className=' w-screen  h-12 flex flex-row items-center justify-center gap-2 p-2'>
+            <p>Hecho por Eze ❤️</p>
+            <a target={'_blank'} href="https://www.linkedin.com/in/ezequiel-r/" >
+            <LinkedInIcon />
+            </a>
+            <a target={'_blank'} href="https://github.com/Quide1" >
+                <GitHubIcon />
+            </a>
+        </footer>
+    )
+}
+
+export default Footer
