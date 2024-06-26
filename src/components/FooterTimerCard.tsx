94d32898b65a5a98eac16f64cf9b253cc4be5225
Quide1 <ezequielr.dev@gmail.com>
2024-06-26 09:47:19 -0300

    change sound

--- src/components/FooterTimerCard.tsx
+++ src/components/FooterTimerCard.tsx
@@ -3,15 +3,26 @@ import { Button } from "./ui/button";
 import { useTimerFunctionalities } from "@/hooks/useTimerFunctionalities";
 import PlayIcon from "@/icons/PlayIcon";
 import PauseIcon from "@/icons/PauseIcon";
+import { playSound } from "@/utils/playSound";
 function FooterTimerCard() {
   const { startTimer, timerStatus, pauseTimer } = useTimerFunctionalities();
+  const onPauseClick = () => {
+    pauseTimer()
+    playSound("public/button-press.mp3")
+  }
+
+  const onStartClick = () => {
+    startTimer()
+    playSound("public/button-press.mp3")
+
+  }
   return (
     <CardFooter>
       {timerStatus.isRunning ? (
-        <Button aria-label="Pausar temporizador" className="bg-gray-800  " onClick={pauseTimer}>
-         Pausar <PauseIcon className={'ml-2'}/></Button>
+        <Button aria-label="Pausar temporizador" className="bg-gray-800  " onClick={onPauseClick}>
+          Pausar <PauseIcon className={'ml-2'} /></Button>
       ) : (
-        <Button  aria-label="Comenzar o reanudar el temporizador"  className="bg-gray-800  " onClick={startTimer}>Comenzar  <PlayIcon className="ml-2"/></Button>
+        <Button aria-label="Comenzar o reanudar el temporizador" className="bg-gray-800  " onClick={onStartClick}>Comenzar  <PlayIcon className="ml-2" /></Button>
       )}
     </CardFooter>
   );
--- src/utils/playSound.ts
+++ src/utils/playSound.ts
@@ -1,5 +1,6 @@
 // utils/soundUtils.ts
 export const playSound = (soundUrl: string) => {
     const audio = new Audio(soundUrl);
+    audio.volume = 0.1
     audio.play();
 };
