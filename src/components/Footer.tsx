5d7416b90ce7e313e5d795f1ccc2455e77af75a0
Quide1 <ezequielr.dev@gmail.com>
2024-08-14 21:55:23 -0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,6 @@ function Footer() {
     return (
         <footer className=' w-full  h-12 flex flex-row items-center justify-center gap-2 p-2 border'>
             <p>Hecho por Eze ❤️</p>
-            <a  aria-label='Conóceme visitando mi linkedin' target={'_blank'} href="https://www.linkedin.com/in/ezequiel-r/" >
-            <LinkedInIcon />
-            </a>
             <a  aria-label='Conoce mas sobre mis proyectos en github' target={'_blank'} href="https://github.com/Quide1" >
                 <GitHubIcon />
             </a>
